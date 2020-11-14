ec4ecfd19822c2f546563613b1f54f2aaad6f3fa
Josh <joshrazonn@gmail.com>
2020-11-13 23:15:43 -0700

    finished hwk

---
+++ public/js/client.js
@@ -0,0 +1,22 @@
+import {hikes} from './hikes.js'
+
+console.log(hikes)
+
+// let outputHTML = "";
+
+// let gallery = document.querySelector('.gallery');
+
+
+// hikes.forEach(function(hike) {
+
+
+//   outputHTML += `
+//     <div class="card">
+//       <img src="./images/${hike.imagePath()}.jpg" height="${hike.height}" width="${hike.width}" alt="Lorem Picsum: ${animal.name}">
+//       <p>${hike.credit}</p>
+//     </div>
+//   `
+  
+//   gallery.innerHTML = outputHTML;
+// })
+
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.render('./pages'));
+app.get('/subscribe', (req, res) => res.render('./pages/subscribe'));
+app.get('/gallery', (req, res) => res.render('./pages/gallery'));
+
+app.use((req, res) => {
+  res.status(404);
+  res.send('404: File Not Found');
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
